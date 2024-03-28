c48c1e1fccacb2ac81793023ea97ce0f1e637357
Nayan Machhi <nayan.machhi@investisdigital.com>
2024-03-28 07:19:59 +0000

    new code updated now

---
+++ javascript/vertical-slider.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
